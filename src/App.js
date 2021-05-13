f391148b54c0dcda3b4d8c9bdba978713cdabacf
yash <ymehta777@gmail.com>
2021-05-13 15:43:25 +0530

    :zap: Finish Banner and Nav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
 import Row from "./Row";
 import requests from "./requests";
+import Banner from "./Banner";
+import Nav from "./Nav";
 
 function App() {
   return (
-    <div>
+    <div className="app">
+      <Nav></Nav>
+      <Banner></Banner>
       <Row
         title="Netflix Originals"
         fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
       ></Row>
       <Row title="Trending Now" fetchUrl={requests.fetchTrending}></Row>
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated}></Row>
---
+++ src/Banner.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import axios from "./axios";
+import requests from "./requests";
+import "./banner.css";
+
+const baseUrl = "https://image.tmdb.org/t/p/original";
+
+const Banner = () => {
+  const [movie, setMovie] = useState([]);
+  useEffect(() => {
+    axios
+      .get(requests.fetchNetflixOriginals)
+      .then((req) =>
+        setMovie(
+          req.data.results[
+            Math.floor(Math.random() * req.data.results.length - 1)
+          ]
+        )
+      );
+  }, []);
+  console.log(movie);
+
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url(${baseUrl}/${movie.backdrop_path})`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <h1 className="banner__description">
+          {truncate(movie?.overview, 150)}
+        </h1>
+      </div>
+      <div className="banner--fadeBottom"></div>
+    </header>
+  );
+};
+
+export default Banner;
